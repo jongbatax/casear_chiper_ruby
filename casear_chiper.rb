d908b00c227a086bf67568be24583ed41015c30e
Herri Siagian <herry.it.2007@gmail.com>
2023-10-25 18:46:17 +0700

    Update casear_chiper string only

--- casear_chiper.rb
+++ casear_chiper.rb
@@ -1,19 +1,17 @@
-def caesar_chiper(string, shift_num)
-  plain = string.downcase.split('')
-  chiper = Array.new(plain.length)
-
-  plain.each_with_index do |c, i|
-    chiper[i] = if c in 'a'..'z'
-                  if c.ord + shift_num > 122
-                    ((c.ord + shift_num) - 26).chr
-                  else
-                    (c.ord + shift_num).chr
-                  end
+def caesar_chiper(plain, shift_num)
+  chiper = ''
+  plain.downcase.each_char do |c|
+    chiper += if c in 'a'..'z'
+                if c.ord + shift_num > 122
+                  ((c.ord + shift_num) - 26).chr
                 else
-                  c
+                  (c.ord + shift_num).chr
                 end
+              else
+                c
+              end
   end
   chiper
 end
 
-puts caesar_chiper('What a string!', 5).join()
+puts caesar_chiper('What a string!', 5)
